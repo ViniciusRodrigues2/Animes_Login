8ff4ad5f88706180cfdde78891e3f5bb8893fae3
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-07-22 17:33:41 -0300

    commit inicial

--- show.js
+++ show.js
@@ -4,14 +4,18 @@
        if(btn.classList.contains('white')){
         document.body.classList.remove('muda');
         btn.classList.remove('white');
+        var imgAnime = document.querySelector('.img-anime');
         var image = document.querySelector('.image');
+        imgAnime.src = 'img/kirito.png';
         image.src = 'img/sun.png';
        }
        else{
         btn.classList.add('white');
         document.body.classList.add('muda')
         var image = document.querySelector('.image');
+        var imgAnime = document.querySelector('.img-anime');
         image.src = 'img/moon.png';
+        imgAnime.src = 'img/asuna-bg.png';
        }
     }
     btn.addEventListener('click',mudarCor)
