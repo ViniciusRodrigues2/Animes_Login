68a4308f3cdf49801128c09ce99c23b04a35053d
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-07-22 14:55:25 -0300

    commit inicial
    treinamento html e css

---
+++ show.js
@@ -0,0 +1,19 @@
+(function(){
+    var btn = document.querySelector('.mude-cor');
+    function mudarCor(){
+       if(btn.classList.contains('white')){
+        document.body.classList.remove('muda')
+        btn.classList.remove('white');
+        var image = document.querySelector('.image');
+        image.src = 'img/sun.png';
+       }
+       else{
+        btn.classList.add('white');
+        document.body.classList.add('muda')
+        var image = document.querySelector('.image');
+        image.src = 'img/moon.png';
+       }
+    }
+    btn.addEventListener('click',mudarCor)
+    
+})();
